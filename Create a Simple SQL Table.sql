06e27dfd067ad5bed7f8ae49ba13530164782eaa
SHIVATHMAJ SHENOY M <54990364+shivathmajshenoym@users.noreply.github.com>
2022-08-11 10:10:35 +0530

    Add files via upload

---
+++ Create a Simple SQL Table.sql
@@ -0,0 +1,14 @@
+REM   Script: 1. Write a SQL statement to create a simple table countries including columns country_id,country_name and region_id.
+REM   1. Write a SQL statement to create a simple table countries including columns
+country_id,country_name and region_id.
+
+CREATE TABLE COUNTRIES 
+( 
+    COUNTRY_ID varchar(2), 
+    COUNTRY_NAME varchar(10), 
+    REGION_ID decimal(10,0) 
+);
+
+DESC COUNTRIES
+
+
